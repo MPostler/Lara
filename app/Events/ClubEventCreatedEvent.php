6669a273b4991c1cedb912a3f743eefb7ce0171e
Markus Postler <markus.postler@tu-ilmenau.de>
2019-08-30 05:13:56 +0200

    Event and Listener logic to send simple push message for new club events

--- app/Events/ClubEventCreatedEvent.php
+++ app/Events/ClubEventCreatedEvent.php
@@ -9,19 +9,24 @@ use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Lara\Section;
 
 class ClubEventCreatedEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $newEvent;
+    public $section;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($newEvent)
     {
-        //
+        $this->newEvent = $newEvent;
+        $this->section = Section::where('id', '=', $newEvent->plc_id)->first();
     }
 
     /**
--- app/Listeners/ClubEventCreatedListener.php
+++ app/Listeners/ClubEventCreatedListener.php
@@ -2,30 +2,35 @@
 
 namespace Lara\Listeners;
 
+use Exception;
 use Lara\Events\ClubEventCreatedEvent;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Pusher\PushNotifications\PushNotifications;
 
 class ClubEventCreatedListener
 {
     /**
-     * Create the event listener.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Handle the event.
      *
-     * @param  ClubEventCreatedEvent  $event
+     * @param ClubEventCreatedEvent $event
      * @return void
+     * @throws Exception
      */
     public function handle(ClubEventCreatedEvent $event)
     {
-        //
+        $beamsClient = new PushNotifications(array(
+            "instanceId" => config('broadcasting.connections.pusher.beams.instance_id'),
+            "secretKey" => config('broadcasting.connections.pusher.beams.secret_key')
+        ));
+
+        $beamsClient->publishToInterests(
+            array(str_replace("é", "e", $event->section->title)),
+            array("fcm" => array("notification" => array(
+                "title" => "A new event for {$event->section->title} was created!",
+                "body" => "Title: {$event->newEvent->evnt_title} "
+            )),
+            )
+        );
     }
 }
