b13c631cfd87b1e83d7a249f9edc743681074eb0
Markus Postler <markus.postler@tu-ilmenau.de>
2019-08-30 05:14:34 +0200

    config files to handle pusher beams

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('BROADCAST_DRIVER', 'null'),
+    'default' => env('BROADCAST_DRIVER', 'pusher'),
 
     /*
     |--------------------------------------------------------------------------
@@ -36,7 +36,12 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                //
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+		        'useTLS' => true
+            ],
+            'beams' => [
+                'secret_key' => env('PUSHER_BEAMS_SECRET_KEY'),
+                'instance_id' => env('PUSHER_BEAMS_INSTANCE_ID'),
             ],
         ],
 
